05282d82501ca9f38486c4a275132cc266fea3d8
JazzBrotha <mattiasostblom@protonmail.com>
2017-04-13 11:32:02 +0200

    Fully responsive, beautified

--- js/script.js
+++ js/script.js
@@ -1,32 +1,28 @@
-// jshint esversion:6
-
-// jQuery
-	$("#search-box").keyup(function(e){
-		let searchValue = $("#search-box").val();
-		$.getJSON("http://en.wikipedia.org/w/api.php?callback=?",
-		{
-			srsearch: searchValue,
-			action: "query",
-			list: "search",
-			format: "json"
-		},
-		function(data) {
-			$("#result-info").empty();
-			if (searchValue.length !== 0) {
-			$("#result-info").append(`<p>Results for "<strong>${searchValue}</strong>"</p>`);
-		}
-			$("#result-list").empty();
-			$.each(data.query.search, function(i, item){
-				$("#result-list").append(
-					`
-					<a href="http://en.wikipedia.org/wiki/${encodeURIComponent(item.title)}" target="_blank" class="list-group-item list-group-item-action flex-column align-items-start">
-						<div class="d-flex w-100 justify-content-between">
-							<h5 class="mb-1"><strong>${item.title}</strong></h5>
-						</div>
-						<p class="mb-1">${item.snippet}</p>
-					</a>
-					`
-				);
-			});
-		});
+	$("#search-box").keyup(function(e) {
+	    let searchValue = $("#search-box").val();
+	    $.getJSON("http://en.wikipedia.org/w/api.php?callback=?", {
+	            srsearch: searchValue,
+	            action: "query",
+	            list: "search",
+	            format: "json"
+	        },
+	        function(data) {
+	            $("#result-info").empty();
+	            if (searchValue.length !== 0) {
+	                $("#result-info").append(`<p>Results for "<strong>${searchValue}</strong>"</p>`);
+	            }
+	            $("#result-list").empty();
+	            $.each(data.query.search, function(i, item) {
+	                $("#result-list").append(
+	                    `
+												<a href="http://en.wikipedia.org/wiki/${encodeURIComponent(item.title)}" target="_blank" class="list-group-item list-group-item-action flex-column align-items-start">
+													<div class="d-flex w-100 justify-content-between">
+														<h5 class="mb-1"><strong>${item.title}</strong></h5>
+													</div>
+													<p class="mb-1">${item.snippet}</p>
+												</a>
+											`
+	                );
+	            });
+	        });
 	});
