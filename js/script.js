78cddefb07efc827ecf4a1d5f70f5065621b72fb
JazzBrotha <JazzBrotha@users.noreply.github.com>
2017-04-13 12:03:31 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,13 +15,13 @@
 	            $.each(data.query.search, function(i, item) {
 	                $("#result-list").append(
 	                    `
-												<a href="http://en.wikipedia.org/wiki/${encodeURIComponent(item.title)}" target="_blank" class="list-group-item list-group-item-action flex-column align-items-start">
-													<div class="d-flex w-100 justify-content-between">
-														<h5 class="mb-1"><strong>${item.title}</strong></h5>
-													</div>
-													<p class="mb-1">${item.snippet}</p>
-												</a>
-											`
+			      <a href="http://en.wikipedia.org/wiki/${encodeURIComponent(item.title)}" target="_blank" class="list-group-item list-group-item-action flex-column align-items-start">
+			        <div class="d-flex w-100 justify-content-between">
+			          <h5 class="mb-1"><strong>${item.title}</strong></h5>
+				</div>
+				  <p class="mb-1">${item.snippet}</p>
+			      </a>
+			    `
 	                );
 	            });
 	        });
